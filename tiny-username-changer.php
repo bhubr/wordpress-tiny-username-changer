c95af4b85f1092c34c0f9eaa06dd06176916beb8
Benoît Hubert <benoithubert@gmail.com>
2018-01-18 12:27:40 +0100

    Fix it

--- tiny-username-changer.php
+++ tiny-username-changer.php
@@ -2,5 +2,11 @@
 /*
 Plugin Name: Tiny Username Changer
 */
-function username_changer_register_my_custom_menu_page() { add_menu_page("Username changer","Username changer","manage_options","tiny-username-changer/tiny-username-changer-form.php","","",6); }
+function username_changer_register_my_custom_menu_page() {
+    add_menu_page( "Username changer", "Username changer", "manage_options", "tiny-username-changer","tiny_username_changer_form","",6);
+}
 add_action( 'admin_menu', 'username_changer_register_my_custom_menu_page' );
+
+function tiny_username_changer_form() {
+  require 'tiny-username-changer-form.php';
+}
